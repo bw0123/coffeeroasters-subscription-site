c2cc214d06d26b9bffc0e2235bd2f6f68a1b00b9
Britney Wu <Wu.Britney@gmail.com>
2023-12-21 11:41:35 -0500

    add collection label, desc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,12 +23,24 @@ function Home() {
                 </div>
             </main>
             <section>
-                <h1 id='collection'>our collection</h1>
+                <h1 id='our-collection'>our collection</h1>
                 <div class='collection-flex-container'>
                     <img src={granespresso} alt='image-gran-espresso'></img>
                     <img src={planalto} alt='image-planalto'></img>
                     <img src={piccollo} alt='image-piccollo'></img>
                     <img src={danche} alt='image-danche'></img>
+                </div>
+                <div class='collection-label'>
+                    <h4>Gran Espresso</h4>
+                    <h4>Planalto</h4>
+                    <h4>Piccollo</h4>
+                    <h4>Danche</h4>
+                </div>
+                <div class='collection-desc'>
+                    <p>Light and flavorful blend with cocoa<br></br>and black pepper for an intense<br></br>experience.</p>
+                    <p>Brazilian dark roast with rich and<br></br>velvety body, and hints of fruits and<br></br>nuts.</p>
+                    <p>Mild and smooth blend featuring<br></br>notes of toasted almond and dried<br></br>cherry.</p>
+                    <p>Ethiopian hand-harvested blend densely packed with vibrant fruit<br></br>notes.</p>
                 </div>
             </section>
         </div>
