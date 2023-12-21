3b435ddb6695f2ac481fafa30608de837e47378e
Britney Wu <Wu.Britney@gmail.com>
2023-12-21 12:32:29 -0500

    cleanup .css styles by removing repeating

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,8 @@
 import Header from './Header'
+import Collection from './Collection'
+import WhyChoose from './WhyChoose'
 import coffeepress from '../assets/img/image-hero-coffeepress.jpg'
-import granespresso from '../assets/icons/image-gran-espresso.png'
-import planalto from '../assets/icons/image-planalto.png'
-import piccollo from '../assets/icons/image-piccollo.png'
-import danche from '../assets/icons/image-danche.png'
+
 
 function Home() {
     return(
@@ -22,27 +21,9 @@ function Home() {
                     </button>
                 </div>
             </main>
-            <section>
-                <h1 id='our-collection'>our collection</h1>
-                <div class='collection-flex-container'>
-                    <img src={granespresso} alt='image-gran-espresso'></img>
-                    <img src={planalto} alt='image-planalto'></img>
-                    <img src={piccollo} alt='image-piccollo'></img>
-                    <img src={danche} alt='image-danche'></img>
-                </div>
-                <div class='collection-label'>
-                    <h4>Gran Espresso</h4>
-                    <h4>Planalto</h4>
-                    <h4>Piccollo</h4>
-                    <h4>Danche</h4>
-                </div>
-                <div class='collection-desc'>
-                    <p>Light and flavorful blend with cocoa<br></br>and black pepper for an intense<br></br>experience.</p>
-                    <p>Brazilian dark roast with rich and<br></br>velvety body, and hints of fruits and<br></br>nuts.</p>
-                    <p>Mild and smooth blend featuring<br></br>notes of toasted almond and dried<br></br>cherry.</p>
-                    <p>Ethiopian hand-harvested blend densely packed with vibrant fruit<br></br>notes.</p>
-                </div>
-            </section>
+            <Collection />
+            <WhyChoose />
+            
         </div>
     )
 }
---
+++ src/components/WhyChoose.jsx
@@ -0,0 +1,15 @@
+function WhyChoose() {
+    return(
+        <section class='why-choose-container'>
+            <div class='why-choose-txt-container'>
+            <h2>Why choose us?</h2>
+                <p>A large part of our role is choosing which particular coffees will be feature<br></br>
+                    in our range. This means working closely with the best coffee growers to give<br></br>
+                    you are more impactful experience on every level.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default WhyChoose
