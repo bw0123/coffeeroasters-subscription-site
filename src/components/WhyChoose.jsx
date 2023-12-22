0506230687651a1b73f13c49f95af42c670dd56d
Britney Wu <Wu.Britney@gmail.com>
2023-12-21 19:45:53 -0500

    create 48px line height

--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -7,7 +7,7 @@ function WhyChoose() {
         <section class='why-choose-container'>
             <div class='why-choose-txt-container'>
                 <h2 id='why-choose-title'>Why choose us?</h2>
-                    <p>A large part of our role is choosing which particular coffees will be feature<br></br>
+                    <p class='why-choose-desc'>A large part of our role is choosing which particular coffees will be feature<br></br>
                         in our range. This means working closely with the best coffee growers to give<br></br>
                         you are more impactful experience on every level.
                     </p>
