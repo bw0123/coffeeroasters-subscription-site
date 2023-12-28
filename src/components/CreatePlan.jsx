b1bb7aa5d243c77fd2ba943060538184a039b3fb
Britney Wu <Wu.Britney@gmail.com>
2023-12-28 11:43:06 -0500

    set proper alignment for dark container

--- src/components/CreatePlan.jsx
+++ src/components/CreatePlan.jsx
@@ -14,6 +14,29 @@ function CreatePlan() {
                         </p>
                 </div>
             </main>
+            <section class='how-container-dark'>
+                <hr></hr>
+                <div class='how-hq-flex-container' id='how-flex-container-dark'>
+                    <div class='how-hq-step-container'>
+                        <StepsOval/>
+                            <h1 class='how-step-number'>01</h1>
+                            <h3 class='how-step-label-dark'>Pick your<br></br>coffee</h3>
+                            <p class='how-step-desc-dark'>Select from our evolving range of artisan<br></br>coffees. Our beans are ethically sourced<br></br>and we pay fair prices for them. There are<br></br>new coffees in all profiles every month<br></br>for you to try out.</p>
+                    </div>
+                    <div class='how-hq-step-container'>
+                        <StepsOval/>
+                            <h1 class='how-step-number'>02</h1>
+                            <h3 class='how-step-label-dark'>Choose the<br></br>frequency</h3>
+                            <p class='how-step-desc-dark'>Customize your order frequency,<br></br>quantity, even your roast style and grind<br></br>type. Pause, skip or cancel your<br></br>subscription with no commitment<br></br>through our online portal.</p>
+                    </div>
+                    <div class='how-hq-step-container'>
+                        <StepsOval/>
+                            <h1 class='how-step-number'>03</h1>
+                            <h3 class='how-step-label-dark'>Receive and<br></br>enjoy!</h3>
+                            <p class='how-step-desc-dark'>We ship your package within 48 hours,<br></br>freshly roasted. Sit back and enjoy<br></br>award-winning world-class coffees<br></br>curated to provide a distinct tasting<br></br>experience.</p>
+                    </div>
+                </div>
+            </section>
             
         </div>
         
