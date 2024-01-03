83be948871f978a3a449ea99c217f07d24d1aa95
Britney Wu <Wu.Britney@gmail.com>
2024-01-03 11:13:29 -0500

    set up select flexbox && alignment. needs styling

--- src/components/AsideStep.jsx
+++
@@ -1,8 +0,0 @@
-export default function AsideStep({ id, label }) {
-    return(
-        <div class='aside-step-container'>
-            <h4 class='aside-step-id'>{id}</h4>
-            <h4 class='aside-step-label'>{label}</h4>
-        </div>
-    )
-}
--- src/components/CreatePlan.jsx
+++ src/components/CreatePlan.jsx
@@ -3,14 +3,6 @@ import Blackcup from '../assets/img/image-hero-blackcup.jpg'
 import StepsOvalDark from '../assets/icons/OvalDarkFill'
 
 function CreatePlan() {
-    const [labelColor, setLabelColor] = useState({ color: '#333D4B', opacity: 0.75 })
-    const [isGreen, setIsGreen] = useState({ color: '#0E8784', opacity: 0.75 })
-
-    const changeStyle = () => {
-        setIsGreen({ color: '#0E8784', opacity: 1 })
-        setLabelColor({ color: '#000000', opacity: 1 })
-    }
-
     return(
         <div>
             <main class='main-bg-container'>
@@ -48,59 +40,59 @@ function CreatePlan() {
                     </div>
                 </div>
             </section>
-            <aside class='aside-step-col'>
-                    <div class='aside-step-container' onClick={changeStyle} id={1}>
-                        <h4 class='aside-step-id' style={isGreen}>01</h4>
-                        <h4 class='aside-step-label' style={labelColor}>Preferences</h4>
-                    </div>
-                    <hr class='aside-hr'></hr>
-                    <div class='aside-step-container' id={2}>
-                        <h4 class='aside-step-id'>02</h4>
-                        <h4 class='aside-step-label'>Bean Type</h4>
-                    </div>
-                    <hr class='aside-hr'></hr>
-                    <div class='aside-step-container'>
-                        <h4 class='aside-step-id'>03</h4>
-                        <h4 class='aside-step-label'>Quantity</h4>
-                    </div>
-                    <hr class='aside-hr'></hr>
-                    <div class='aside-step-container'>
-                        <h4 class='aside-step-id'>04</h4>
-                        <h4 class='aside-step-label'>Grind Option</h4>
-                    </div>
-                    <hr class='aside-hr'></hr>
-                    <div class='aside-step-container'>
-                        <h4 class='aside-step-id'>05</h4>
-                        <h4 class='aside-step-label'>Deliveries</h4>
-                    </div>
-            </aside>
-            {/*
-            <section class='plan-col'>
-                <div class='select-container'>
-                    <h2 class='select-question'>How do you drink your coffee?</h2>
-                    <div class='select-flex-container'>
-                        <div class='select-card-container'>
-                            <div class='select-card-info'>
-                                <h4 class='select-label'>Capsule</h4>
-                                <p class='select-desc'>Compatible with<br></br>Nespresso systems and<br></br>similar brewers</p>
+            <div class='aside-plan-flex-container'>
+                <aside class='aside-step-col'>
+                        <div class='aside-step-container'>
+                            <h4 class='aside-step-id' >01</h4>
+                            <h4 class='aside-step-label' >Preferences</h4>
+                        </div>
+                        <hr class='aside-hr'></hr>
+                        <div class='aside-step-container'>
+                            <h4 class='aside-step-id'>02</h4>
+                            <h4 class='aside-step-label'>Bean Type</h4>
+                        </div>
+                        <hr class='aside-hr'></hr>
+                        <div class='aside-step-container'>
+                            <h4 class='aside-step-id'>03</h4>
+                            <h4 class='aside-step-label'>Quantity</h4>
+                        </div>
+                        <hr class='aside-hr'></hr>
+                        <div class='aside-step-container'>
+                            <h4 class='aside-step-id'>04</h4>
+                            <h4 class='aside-step-label'>Grind Option</h4>
+                        </div>
+                        <hr class='aside-hr'></hr>
+                        <div class='aside-step-container'>
+                            <h4 class='aside-step-id'>05</h4>
+                            <h4 class='aside-step-label'>Deliveries</h4>
+                        </div>
+                </aside>
+                <section class='plan-col'>
+                    <div class='select-container'>
+                        <h2 class='select-question'>How do you drink your coffee?</h2>
+                        <div class='select-flex-container'>
+                            <div class='select-card-container'>
+                                <div class='select-card-info'>
+                                    <h4 class='select-label'>Capsule</h4>
+                                    <p class='select-desc'>Compatible with<br></br>Nespresso systems and<br></br>similar brewers</p>
+                                </div>
+                            </div>
+                            <div class='select-card-container'>
+                                <div class='select-card-info'>
+                                    <h4 class='select-label'>Filter</h4>
+                                    <p class='select-desc'>Pour over or drip<br></br>methods like Aeropress,<br></br>Chemex, and V60</p>
+                                </div>
+                            </div>
+                            <div class='select-card-container'>
+                                <div class='select-card-info'>
+                                    <h4 class='select-label'>Espresso</h4>
+                                    <p class='select-desc'>Dense and finely ground<br></br>beans for an intense,<br></br>flavorful experience</p>
+                                </div>
                             </div>
                         </div>
-                        <div class='select-card-container'>
-                            <div class='select-card-info'>
-                                <h4 class='select-label'>Filter</h4>
-                                <p class='select-desc'>Pour over or drip<br></br>methods like Aeropress,<br></br>Chemex, and V60</p>
-                            </div>
-                        </div>
-                        <div class='select-card-container'>
-                            <div class='select-card-info'>
-                                <h4 class='select-label'>Espresso</h4>
-                                <p class='select-desc'>Dense and finely ground<br></br>beans for an intense,<br></br>flavorful experience</p>
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </section>
-        */}
+                </section>
+            </div>
         </div>
     )
 }
