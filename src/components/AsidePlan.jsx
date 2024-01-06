d393ddf5ac7fc2bf1e0554cc31c702721873eade
Britney Wu <Wu.Britney@gmail.com>
2024-01-06 16:24:24 -0500

    organize component select

--- src/components/AsidePlan.jsx
+++ src/components/AsidePlan.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-import SelectPlan from './SelectPlan'
+import SelectCard from '../utils/SelectCard'
 
 function AsidePlan() {
+    const [activeId, setActiveId] = useState(1)
+    const [show, setShow] = useState(true)
+
     const numbers = [
         {id: 1, text: "01" },
         {id: 2, text: "02"},
@@ -18,52 +21,35 @@ function AsidePlan() {
         "Deliveries"
     ]
 
-    const [activeId, setActiveId] = useState(1)
-
     const listNumbers = numbers.map(num => 
         <>
-        <div class='aside-step-container' onClick={() => setActiveId(num.id)}>
-            <h4 class='aside-step-id' style={{ color: activeId === num.id ? '#0E8784' : '#83888F' }}>{num.text}</h4>
-            <h4 class='aside-step-label' style={{ color: activeId === num.id ? '#333D4B': '#83888F' }}>{asideLabels[num.id - 1]}</h4>
-        </div>
-        <hr class='aside-hr'></hr>
+            <div class='aside-step-container' onClick={() => setActiveId(num.id)}>
+                <h4 class='aside-step-id' style={{ color: activeId === num.id ? '#0E8784' : '#83888F' }}>{num.text}</h4>
+                <h4 class='aside-step-label' style={{ color: activeId === num.id ? '#333D4B': '#83888F' }}>{asideLabels[num.id - 1]}</h4>
+            </div>
+            <hr class='aside-hr'></hr>
         </>
     )
+
+    const toggle = () => {
+        setShow(!show)
+    }
 
     return(
         <div class='aside-plan-flex-container'>
             <aside class='aside-step-col'>
                 {listNumbers}
             </aside>
-            <SelectPlan />
+            <section class='select-col'>
+                <div class='select-intro-flex-container'>
+                    <h2 class='select-question'>How do you drink your coffee?</h2>
+                    <p>
+                        <i class='arrow up' style={{ transform: show ? 'rotate(45deg)' : 'rotate(-135deg)' }}  onClick={toggle}></i>
+                    </p>
+                </div>
+                {show && <SelectCard />}   
+            </section>
         </div>
     )
-    {/*
-                <div class='aside-step-container'>
-                    <h4 class='aside-step-id'>01</h4>
-                    <h4 class='aside-step-label'>Preferences</h4>
-                </div>
-                <hr class='aside-hr'></hr>
-                <div class='aside-step-container'>
-                    <h4 class='aside-step-id'>02</h4>
-                    <h4 class='aside-step-label'>Bean Type</h4>
-                </div>
-                <hr class='aside-hr'></hr>
-                <div class='aside-step-container'>
-                    <h4 class='aside-step-id'>03</h4>
-                    <h4 class='aside-step-label'>Quantity</h4>
-                </div>
-                <hr class='aside-hr'></hr>
-                <div class='aside-step-container'>
-                    <h4 class='aside-step-id'>04</h4>
-                    <h4 class='aside-step-label'>Grind Option</h4>
-                </div>
-                <hr class='aside-hr'></hr>
-                <div class='aside-step-container'>
-                    <h4 class='aside-step-id'>05</h4>
-                    <h4 class='aside-step-label'>Deliveries</h4>
-                </div>
-            </aside>
-    */}
 }
 export default AsidePlan
