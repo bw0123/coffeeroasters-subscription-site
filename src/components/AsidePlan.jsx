85ced4d3e9ede009b602674a84966ef54a1189e3
Britney Wu <Wu.Britney@gmail.com>
2024-03-28 17:42:27 -0400

    remove extra function in AsidePlan

--- src/components/AsidePlan.jsx
+++ src/components/AsidePlan.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react'
-import SelectCard from '../utils/SelectCard'
-import { selectData } from '../utils/selectData'
 
 function AsidePlan() {
     const [activeId, setActiveId] = useState(1)
     const [isActive, setIsActive] = useState(true)
-
-    const handleBoth = (id) => {
-        setActiveId(id)
-    }
+    const [preference, setPreference] = useState('')
 
     const numbers = [
         {id: 1, text: "01" },
@@ -28,7 +23,7 @@ function AsidePlan() {
 
     const listNumbers = numbers.map((num) => 
         <>
-            <div class='aside-step-container' onClick={() => handleBoth(num.id)} key={num.id}>
+            <div class='aside-step-container' onClick={() => setActiveId(num.id)} key={num.id}>
                 <h4 class='aside-step-id' style={{ color: activeId === num.id ? '#0E8784' : '#83888F' }}>{num.text}</h4>
                 <h4 class='aside-step-label' style={{ color: activeId === num.id ? '#333D4B': '#83888F' }}>{asideLabels[num.id - 1]}</h4>
             </div>
@@ -36,25 +31,47 @@ function AsidePlan() {
         </>
     )
 
-    const listSelectData = selectData.map((select) => 
-        <SelectCard 
-            {...select} key={select.id}
-            onShow={() => setActiveId(select.id)} 
-            isActive={activeId === select.id}
-        />  
-    )
-
     return(
         <div class='aside-plan-flex-container'>
             <aside class='aside-step-col'>
                 {listNumbers}
             </aside>
+
             <section class='select-col'>
-                {listSelectData}
+                <div>
+                    <div class='select-intro-flex-container'>
+                        <h2 class='select-question'>How do you drink your coffee?</h2>
+                        <p>
+                            <i class='arrow up' style={{ transform: activeId === 1 ? 'rotate(45deg)' : 'rotate(-135deg)' }}></i>
+                        </p>
+                    </div>
+                    {activeId === 1 && (
+                        <div class='select-flex-container'>
+                        <div class='select-card-container'>
+                            <div class='select-card-info'>
+                                <h4 class='select-label'>Capsule</h4>
+                                <p class='select-desc'>Compatible with Nespresso systems and similar brewers</p>
+                            </div>
+                        </div>
+                        <div class='select-card-container'>
+                            <div class='select-card-info'>
+                                <h4 class='select-label'>Filter</h4>
+                                <p class='select-desc'>Pour over or drip methods like Aeropress, Chemex, and V60</p>
+                            </div>
+                        </div>
+                        <div class='select-card-container'>
+                            <div class='select-card-info'>
+                                <h4 class='select-label'>Espresso</h4>
+                                <p class='select-desc'>Dense and finely ground beans for an intense, flavorful experience</p>
+                            </div>
+                        </div>
+                    </div>
+                    )}
+                </div>
                 <div class='order-summary-container'>
                     <div class='order-summary-content'>
                         <p>ORDER SUMMARY</p>
-                        <h4>"I drink my coffee as _, with a _ type of bean. _ ground ala _, sent to me _."</h4>
+                        <h4>"I drink my coffee as {preference}, with a _ type of bean. _ ground ala _, sent to me _."</h4>
                     </div>
                 </div>
             </section>
